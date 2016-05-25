16882a93603e1ad146e7f52fef6c6a0d1750a711
coder-bin <569646173@qq.com>
2016-05-25 15:07:22 +0800

    modify something

--- TestGit/TestGitDlg.h
+++ TestGit/TestGitDlg.h
@@ -32,39 +32,5 @@ protected:
 public:
 	afx_msg void OnBnClickedOk();
 	afx_msg void OnBnClickedCancel();
-
-// TestGitDlg.h : header file
-//
-
-#pragma once
-
-
-// CTestGitDlg dialog
-class CTestGitDlg : public CDialogEx
-{
-// Construction
-public:
-	CTestGitDlg(CWnd* pParent = NULL);	// standard constructor
-
-// Dialog Data
-	enum { IDD = IDD_TESTGIT_DIALOG };
-
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
-
-
-// Implementation
-protected:
-	HICON m_hIcon;
-
-	// Generated message map functions
-	virtual BOOL OnInitDialog();
-	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
-	DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedCancel();
 	afx_msg void OnBnClickedButtonTestshow();
 };
